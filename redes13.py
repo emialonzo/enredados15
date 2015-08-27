8ca4aa8c0c7c3369e9b7a08aeaaee309d9236403
Emiliano Alonzo Fulchi <emiliano.alonzo@fing.edu.uy>
2015-08-27 20:44:25 -0300

    comenzando a chequear errores 	modified:   redes13.py

--- redes13.py
+++ redes13.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python
 import subprocess
 import re
+import sys
 
-pingCount="3"
-
-# strMatch="La casa amarilla La pinta lindo"
-# print "probando expresiones regulares con python: "+strMatch
-# print re.match(r'(La)', strMatch, flags=0).group()
-# exit(0)
-
+## Funciones
 def max_ping(output):
     patron = re.compile('(?P<info>.*)=(\s)(?P<min>[^/]+)/(?P<promedio>[^/]+)/(?P<max>[^/]+)/([^/]+)')
     matcher = patron.search(output)
@@ -21,19 +16,53 @@ def dic_ping(output):
 
 def ejecutarPing(host, pingCount="1"):
     cmdping = "ping -c " + pingCount+"  " + host  + " "
-    output = subprocess.check_output(cmdping, shell=True)
+    output = None
+    try:
+        output = subprocess.check_output(cmdping, shell=True)
+        # output = subprocess.check_output(["smartctl", "-A", "/dev/sda2"])
+    except CalledProcessError as e:
+        output = e.output
     print "Salida:::" + output
     patron = re.compile('(?P<info>.*)=(\s)(?P<min>[^/]+)/(?P<promedio>[^/]+)/(?P<max>[^/]+)/([^/]+)')
     matcher = patron.search(output)
     print "Max:::" + max_ping(output)
     print "Max:::" + dic_ping(output)['min']
+    return output
 
+def errorPing(output):
+    patron = re.compile('(ping: unknown host(?P<desconocido>.*)|From (?P<inalcanzable>.*) icmp(.*)Destination Host Unreachable)')
+    matcher = patron.search(output)
+    if(matcher):
+        return (matcher.group('desconocido'),matcher.group('inalcanzable'))
+    else:
+        return (None,None)
+
+
+cantParam = len(sys.argv)
+if(cantParam<2):
+    print 'Error'
+    print 'Uso: python ' + sys.argv[0] + ' host1 host2 [count]'
+    exit(1)
+
+#Traigo parametros
+hostUno = sys.argv[1]
+hostDos = sys.argv[2]
+pingCount='3'
+if cantParam>3:
+    pingCount = sys.argv[3]
+
+
+print "------"
+salida=ejecutarPing(hostUno,pingCount)
+(desconocido,inalcanzable)=errorPing(salida)
+if desconocido or inalcanzable:
+    print 'error con (' + desconocido + ',' + inalcanzable + ')'
 
 print "------"
 ejecutarPing("google.com", pingCount)
 print "------"
 
-cmdping = "ping -c 1  google.com | grep max"
-
-output = subprocess.check_output(cmdping, shell=True)
-print output
+# cmdping = "ping -c 1  google.com | grep max"
+#
+# output = subprocess.check_output(cmdping, shell=True)
+# print output
