dbb4da70e2be8784ee82c9067251c770d4189aba
Emiliano Alonzo Fulchi <emiliano.alonzo@fing.edu.uy>
2015-08-28 00:54:19 -0300

    corregido problema con salida estandar

--- redes13.py
+++ redes13.py
@@ -2,37 +2,56 @@
 import subprocess
 import re
 import sys
+import time
 
 ## Funciones
-def max_ping(output):
+def  max_ping(output):
     patron = re.compile('(?P<info>.*)=(\s)(?P<min>[^/]+)/(?P<promedio>[^/]+)/(?P<max>[^/]+)/([^/]+)')
     matcher = patron.search(output)
-    return matcher.group('max')
+    if matcher:
+        return matcher.group('max')
+    else:
+        return ""
+
 
 def dic_ping(output):
     patron = re.compile('(?P<info>.*)=(\s)(?P<min>[^/]+)/(?P<promedio>[^/]+)/(?P<max>[^/]+)/([^/]+)')
     matcher = patron.search(output)
-    return {'min':matcher.group('min'),'promedio':matcher.group('promedio'),'max':matcher.group('max')}
+    if matcher:
+        return {'min': matcher.group('min'),'promedio': matcher.group('promedio'),'max': matcher.group('max')}
+    else:
+        return {'min': "", 'promedio': "", 'max': ""}
+
 
 def ejecutarPing(host, pingCount="1"):
-    cmdping = "ping -c " + pingCount+"  " + host  + " "
+    cmdping = "ping -c " + pingCount + "  " + host + " "
     output = None
+    # output = subprocess.check_output(cmdping, shell=True)
     try:
-        output = subprocess.check_output(cmdping, shell=True)
+        output = subprocess.check_output(cmdping, shell= True, stderr= subprocess.STDOUT)
+        print ">" + output
         # output = subprocess.check_output(["smartctl", "-A", "/dev/sda2"])
-    except CalledProcessError as e:
+    except Exception as e:
+        # //time.sleep(30)
         output = e.output
-    print "Salida:::" + output
-    patron = re.compile('(?P<info>.*)=(\s)(?P<min>[^/]+)/(?P<promedio>[^/]+)/(?P<max>[^/]+)/([^/]+)')
-    matcher = patron.search(output)
+        # print "<" + output + ">"
+        # if output == "":
+        #     output = "ping: unknown host " + host
+    # print ":::::::"
+    # print output
+    # print ":::::::"
+    # patron = re.compile('(?P<info>.*)=(\s)(?P<min>[^/]+)/(?P<promedio>[^/]+)/(?P<max>[^/]+)/([^/]+)')
+    # matcher = patron.search(output)
     print "Max:::" + max_ping(output)
-    print "Max:::" + dic_ping(output)['min']
+    print "Min:::" + dic_ping(output)['min']
     return output
 
+
 def errorPing(output):
-    patron = re.compile('(ping: unknown host(?P<desconocido>.*)|From (?P<inalcanzable>.*) icmp(.*)Destination Host Unreachable)')
+    patron = re.compile('(ping: unknown host(?P<desconocido>.*))|(From (?P<inalcanzable>.*) icmp(.*)Destination Host Unreachable)')
     matcher = patron.search(output)
     if(matcher):
+        print "HEY!! ERROR"
         return (matcher.group('desconocido'),matcher.group('inalcanzable'))
     else:
         return (None,None)
@@ -54,15 +73,9 @@ if cantParam>3:
 
 print "------"
 salida=ejecutarPing(hostUno,pingCount)
+print ":)" + salida
 (desconocido,inalcanzable)=errorPing(salida)
-if desconocido or inalcanzable:
-    print 'error con (' + desconocido + ',' + inalcanzable + ')'
-
-print "------"
-ejecutarPing("google.com", pingCount)
-print "------"
-
-# cmdping = "ping -c 1  google.com | grep max"
-#
-# output = subprocess.check_output(cmdping, shell=True)
-# print output
+if desconocido:
+    print 'Error: Url desconocida ' + desconocido
+if inalcanzable:
+    print 'Error: Destino inalcanzable ' + inalcanzable
