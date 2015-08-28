62bb00fc7776c9ab73a6a112e6145b1784e0f867
Emiliano Alonzo Fulchi <emiliano.alonzo@fing.edu.uy>
2015-08-28 01:23:41 -0300

    Fin parte 1.3 :)

--- redes13.py
+++ redes13.py
@@ -26,24 +26,24 @@ def dic_ping(output):
 def ejecutarPing(host, pingCount="1"):
     cmdping = "ping -c " + pingCount + "  " + host + " "
     output = None
-    # output = subprocess.check_output(cmdping, shell=True)
     try:
         output = subprocess.check_output(cmdping, shell= True, stderr= subprocess.STDOUT)
-        print ">" + output
+        # print ">" + output
         # output = subprocess.check_output(["smartctl", "-A", "/dev/sda2"])
     except Exception as e:
-        # //time.sleep(30)
         output = e.output
         # print "<" + output + ">"
         # if output == "":
         #     output = "ping: unknown host " + host
+    print "host:" + host
+    print "Ping:" + output.strip()
     # print ":::::::"
     # print output
     # print ":::::::"
     # patron = re.compile('(?P<info>.*)=(\s)(?P<min>[^/]+)/(?P<promedio>[^/]+)/(?P<max>[^/]+)/([^/]+)')
     # matcher = patron.search(output)
-    print "Max:::" + max_ping(output)
-    print "Min:::" + dic_ping(output)['min']
+    # print "Max:::" + max_ping(output)
+    # print "Min:::" + dic_ping(output)['min']
     return output
 
 
@@ -56,7 +56,28 @@ def errorPing(output):
     else:
         return (None,None)
 
+def mejorPromedioq(salida1, salida2):
+    dictPing1 = dic_ping(salida1)
+    dictPing2 = dic_ping(salida2)
+    if float(dictPing1['promedio']) > float(dictPing2['promedio']):
+        return 1
+    else:
+        return 2
 
+
+def procesarErrores(salida):
+    (desconocido,inalcanzable)=errorPing(salida)
+    if desconocido:
+        print 'Error: Url desconocida ' + desconocido
+    if inalcanzable:
+        print 'Error: Destino inalcanzable ' + inalcanzable
+    dictPing = dic_ping(salida)
+    if float(dic_ping(salida)['max']) > umbral:
+        raise Exception('Se ha superado el umbral de tiempo de ' + str(umbral) +'ms')
+
+
+
+######################## MAIN #####################
 cantParam = len(sys.argv)
 if(cantParam<2):
     print 'Error'
@@ -67,13 +88,26 @@ if(cantParam<2):
 hostUno = sys.argv[1]
 hostDos = sys.argv[2]
 pingCount='3'
+umbral=50.00
 if cantParam>3:
     pingCount = sys.argv[3]
 
+if cantParam>4:
+    umbral = float(sys.argv[4])
 
-print "------"
+while True:
+    try:
+        salidaUno=ejecutarPing(hostUno,pingCount)
+        procesarErrores(salidaUno)
+        salidaDos=ejecutarPing(hostDos,pingCount)
+        procesarErrores(salidaDos)
+    except Exception as e:
+        print "Error: " + e.message
+        print 'Cerrando'
+        exit(1)
+
+
 salida=ejecutarPing(hostUno,pingCount)
-print ":)" + salida
 (desconocido,inalcanzable)=errorPing(salida)
 if desconocido:
     print 'Error: Url desconocida ' + desconocido
