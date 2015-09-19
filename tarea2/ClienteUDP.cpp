e1c98a0f731757fc50b2e2f7a57941180ca78a11
rdamiano <rdamiano@mail.com>
2015-09-19 14:21:23 -0300

    modifique el cliente y el servidor, ahora se comunican entre ellos

--- tarea2/ClienteUDP.cpp
+++ tarea2/ClienteUDP.cpp
@@ -13,77 +13,119 @@
 
 using namespace std;
 
-#define HELLO_PORT 8888
-#define HELLO_GROUP "225.5.4.48"
+#define IP_MULTICAST "225.5.4.48"
+#define PUERTO_MULTICAST 8888
+
+#define IP_CLIENTE "127.0.0.1"
+#define PUERTO_CLIENTE 54321
+#define USUARIO "usuario"
+
+#define IP_SERVIDOR "127.0.0.1"
+#define PUERTO_SERVIDOR 51234
+
 #define MSGBUFSIZE 256
 
 int main(int argc, char *argv[]) {
-	string source_iface;
-	string group(HELLO_GROUP);
-	int port(HELLO_PORT);
 
-	if (!(argc < 2)) group = argv[1];
-	if (!(argc < 3)) port = atoi(argv[2]);
-	if (!(argc < 4)) source_iface = argv[3];
+	cout << "Cant Argumentos: " << argc << endl;
 
-	cout << "group: " << group << " port: " << port << " source_iface: " << source_iface << endl;
+	string usuario(USUARIO);
 
+	string ip_multicast(IP_MULTICAST);
+	int puerto_multicast(PUERTO_MULTICAST);
 
-	int sock_multicast;
+	string ip_cliente(IP_CLIENTE);
+	int puerto_cliente(PUERTO_CLIENTE);
+
+	string ip_servidor(IP_SERVIDOR);
+	int puerto_servidor(PUERTO_SERVIDOR);
+
+	if (argc == 2) {
+		cout << "Argumentos: " << argv[0] << " - " << argv[1] << endl;
+		usuario = argv[1];
+	}
+	if (argc == 3) {
+		cout << "Argumentos: " << argv[0] << " - " << argv[1] << " - " << argv[2] << endl;
+		usuario = argv[1];
+		puerto_cliente = atoi(argv[2]);
+	}
+	cout << "Nombre Usuario: " << usuario << endl;
+	cout << "IP Usuario: " << ip_cliente << endl;
+	cout << "Puerto: " << puerto_cliente << endl;
+	cout << "IP Multicast: " << ip_multicast << endl;
+	cout << "Puerto Multicast: " << puerto_multicast << endl;
+
 	//sock_multicast = socket(domain, type, protocol)
 	// domain = AF_INET (IP address family)
 	//type = SOCK_DGRAM (datagram service)
 	//protocol = 0 (variaciones del protocolo)
 	//Creo el socket para usar UDP y datagramas
-	if ((sock_multicast = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+	int socket_cliente = socket(AF_INET, SOCK_DGRAM, 0);
+	if (socket_cliente < 0) {
 		perror("No se pudo crear el socket");
 		exit(0);
 	}
-
-	// Hay que ver que es y si se precisa para algo
-	u_int yes = 1;
-	if (setsockopt(sock_multicast, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
-		perror("Reusing ADDR failed");
-		exit(1);
-	}
+	cout << "Se creo bien el socket" << endl;
 
 	// defino la direccion del socket
 	struct sockaddr_in addr;
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(port);
+	addr.sin_port = htons(puerto_cliente);
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	//addr.sin_addr.s_addr = (group.empty() ? htonl(INADDR_ANY) : inet_addr(group.c_str()));
 
-	if (bind(sock_multicast,(struct sockaddr *)&addr, sizeof(addr)) < 0) {
+	if (bind(socket_cliente,(struct sockaddr *)&addr, sizeof(addr)) < 0) {
 		perror("bind");
 		exit(1);
 	}
+	cout << "Se bindeo bien el socket" << endl;
 
 	struct ip_mreq mreq;
-	mreq.imr_multiaddr.s_addr = inet_addr(group.c_str());
-	mreq.imr_interface.s_addr = (source_iface.empty() ? htonl(INADDR_ANY) : inet_addr(source_iface.c_str()));
+	mreq.imr_multiaddr.s_addr = inet_addr(ip_multicast.c_str());
+	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
 
-    if (setsockopt(sock_multicast, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
+  if (setsockopt(socket_cliente, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
 		perror("setsockopt");
 		exit(1);
 	}
+	cout << "se setearon bien las opciones." << endl;
 
 	socklen_t addrlen;
 	int nbytes;
 	char msgbuf[MSGBUFSIZE];
 
 	while (1) {
+
+		char buffer[MSGBUFSIZE];
+	  bzero(buffer,MSGBUFSIZE);
+	  fgets(buffer,MSGBUFSIZE,stdin);
+
+		printf("este es el mensaje que le envio al servidor: %s\n",buffer);
+
+		struct sockaddr_in addr_servidor;
+	  memset((char *)&addr_servidor, 0, sizeof(addr_servidor));
+	  addr_servidor.sin_family = AF_INET;
+	  addr_servidor.sin_port = htons(puerto_servidor);
+	  addr_servidor.sin_addr.s_addr = inet_addr(ip_servidor.c_str());
+
+		int result = sendto(socket_cliente, buffer, strlen(buffer), 0, (struct sockaddr *)&addr_servidor, sizeof(addr_servidor));
+
+	  if (result < 0) {
+	    cout << "algo no salio bien cuando mandamos el mensaje." << endl;
+	    perror("algo no salio bien cuando mandamos el mensaje.");
+	    exit(1);
+	  } else {
+	    printf("se enviaron %d bytes\n", result);
+	  }
+
 		memset(&msgbuf, 0, MSGBUFSIZE);
 
 		addrlen = sizeof(addr);
-		if ((nbytes = recvfrom(sock_multicast, msgbuf, MSGBUFSIZE, 0, (struct sockaddr *)&addr, &addrlen)) < 0) {
+		if ((nbytes = recvfrom(socket_cliente, msgbuf, MSGBUFSIZE, 0, (struct sockaddr *)&addr, &addrlen)) < 0) {
 			perror("recvfrom");
 			exit(1);
 		}
-		printf("Mensaje recibido: %s\n", msgbuf);
-		// cout.write(msgbuf, nbytes);
-		// cout.flush();
+		printf("Mensaje recibido desde el servidor: %s\n", msgbuf);
 	}
 	return 0;
 }
--- tarea2/ServidorUDP.cpp
+++ tarea2/ServidorUDP.cpp
@@ -13,14 +13,21 @@
 
 using namespace std;
 
-#define PUERTO 54321;
+#define IP_SERVIDOR "127.0.0.1"
+#define PUERTO_SERVIDOR 51234
+
 #define IP_MULTICAST "225.5.4.48"
+#define PUERTO_MULTICAST 8888
+
 #define MSGBUFSIZE 256
 
 int main (int argc, char *argv[]) {
 
+  string ip_servidor(IP_SERVIDOR);
+  int puerto_servidor(PUERTO_SERVIDOR);
+
   string ip_multicast(IP_MULTICAST);
-  int puerto(puerto);
+  int puerto_multicast(PUERTO_MULTICAST);
 
   // Abro el socket UDP/IP
   int socket_servidor;
@@ -33,7 +40,7 @@ int main (int argc, char *argv[]) {
   struct sockaddr_in addr;
   memset((char *)&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(puerto);
+  addr.sin_port = htons(puerto_servidor);
   addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
   if (bind(socket_servidor,(struct sockaddr *)&addr, sizeof(addr)) < 0) {
@@ -42,28 +49,44 @@ int main (int argc, char *argv[]) {
   }
   cout << "Se bindeo bien el socket" << endl;
 
-  char buffer[MSGBUFSIZE];
-  bzero(buffer,MSGBUFSIZE);
-  fgets(buffer,MSGBUFSIZE,stdin);
+  socklen_t addrlen;
+  int nbytes;
+  char msgbuf[MSGBUFSIZE];
 
-  // char* mensajePrueba = "Este es un mensaje de prueba para mandar al multicast";
-  // char * aux = (char *) mensajePrueba.c_str();
-  printf("este es el mensaje: %s\n",buffer);
+  while(1) {
+    char buffer[MSGBUFSIZE];
+    bzero(buffer,MSGBUFSIZE);
+    //fgets(buffer,MSGBUFSIZE,stdin);
 
-  struct sockaddr_in addr_multicast;
-  memset((char *)&addr_multicast, 0, sizeof(addr_multicast));
-  addr_multicast.sin_family = AF_INET;
-  addr_multicast.sin_port = htons(8888);
-  addr_multicast.sin_addr.s_addr = inet_addr(ip_multicast.c_str());
+    memset(&msgbuf, 0, MSGBUFSIZE);
+    addrlen = sizeof(addr);
+    if ((nbytes = recvfrom(socket_servidor, msgbuf, MSGBUFSIZE, 0, (struct sockaddr *)&addr, &addrlen)) < 0) {
+			perror("recvfrom");
+			exit(1);
+		}
+		printf("Mensaje recibido del cliente: %s\n", msgbuf);
 
-  int result = sendto(socket_servidor, buffer, strlen(buffer), 0, (struct sockaddr *)&addr_multicast, sizeof(addr_multicast));
+    // char* mensajePrueba = "Este es un mensaje de prueba para mandar al multicast";
+    // char * aux = (char *) mensajePrueba.c_str();
 
-  if (result < 0) {
-    cout << "algo no salio bien cuando mandamos el mensaje." << endl;
-    perror("algo no salio bien cuando mandamos el mensaje.");
-    exit(1);
-  } else {
-    printf("se enviaron %d bytes\n", result);
+    strcat(buffer, "pepito: ");
+    strcat(buffer, msgbuf);
+
+    struct sockaddr_in addr_multicast;
+    memset((char *)&addr_multicast, 0, sizeof(addr_multicast));
+    addr_multicast.sin_family = AF_INET;
+    addr_multicast.sin_port = htons(puerto_multicast);
+    addr_multicast.sin_addr.s_addr = inet_addr(ip_multicast.c_str());
+
+    int result = sendto(socket_servidor, buffer, strlen(buffer), 0, (struct sockaddr *)&addr_multicast, sizeof(addr_multicast));
+
+    if (result < 0) {
+      cout << "algo no salio bien cuando mandamos el mensaje." << endl;
+      perror("algo no salio bien cuando mandamos el mensaje.");
+      exit(1);
+    } else {
+      printf("se enviaron %d bytes\n", result);
+    }
   }
 
   close(socket_servidor);
